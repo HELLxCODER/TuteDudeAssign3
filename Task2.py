1754cc442b1fb10be6bcba1e6d950e0d572c4ced
HELLxCODER <pranavvasisth10@gmail.com>
2025-09-24 20:04:21 +0530

    Initial Commit

---
+++ Task2.py
@@ -0,0 +1,8 @@
+import math
+def calculation():
+    a = int(input("Enter the number that you want the calculations to be done: "))
+    print(f"The square root of {a} is {math.sqrt(a)}")
+    print(f"The logarithm of {a} is {math.log(a)}")
+    print(f"The sine of {a} is {math.sin(a)}")
+
+calculation() 
